053f978c8b7807717e18e3b2b63eedebb79f5bec
king525dev <109004601+king525dev@users.noreply.github.com>
2022-12-31 03:11:39 +0100

    Added Status update to "Notes"
    Added status update to "Notes" and other minor style updates

--- javascript/notes.js
+++ javascript/notes.js
@@ -2,12 +2,42 @@
      [  NOTES  ].html
 =============================*/ 
 
-const deter = true;
 const title = document.getElementById("notes-title");
 const body = document.getElementById("notes-body");
 const deleteBtn = document.getElementById("delete-btn");
 const saveBtn = document.getElementById("save-btn");
 let notesList = document.getElementById("notes-list");
+
+//Getting Status
+function statusOutput(){
+     const todoOption = document.getElementById("Todo");
+     const doingOption = document.getElementById("Doing");
+     const doneOption = document.getElementById("Done");
+     if(todoOption.checked){
+          return "Todo";
+     }else if(doingOption.checked){
+          return "Doing";
+     }else if(doneOption.checked){
+          return "Done";
+     }else{
+          return "Todo";
+     }
+}
+
+function checkingStatus(status){
+     const todoOption = document.getElementById("Todo");
+     const doingOption = document.getElementById("Doing");
+     const doneOption = document.getElementById("Done");
+     if(status == "Todo"){
+          todoOption.checked = true;
+     }else if(status == "Doing"){
+          doingOption.checked = true;
+     }else if(status == "Done"){
+          doneOption.checked = true
+     }else{
+          todoOption.checked = true;
+     }
+}
 
 //Date 
 const dater = new Date();
@@ -57,24 +87,33 @@ function renderData(individualDoc){
      }
 
      let parentDiv = document.createElement("div");
-     parentDiv.className= "notes-list-item";
+     parentDiv.className = "notes-list-item";
      parentDiv.setAttribute("data-id", individualDoc.id);
 
      let noteTitle = document.createElement("div");
-     noteTitle.className= "notes-small-title";
+     noteTitle.className = "notes-small-title";
      noteTitle.textContent = individualDoc.data().title;
 
      let noteContent = document.createElement("div");
-     noteContent.className= "notes-small-body";
-     noteContent.textContent = contentShortening(individualDoc.data().note) ;
+     noteContent.className = "notes-small-body";
+     noteContent.textContent = contentShortening(individualDoc.data().note);
+
+     let noteFooter = document.createElement("div");
+     noteFooter.className = "small-bottom";
+
+     let noteStatus = document.createElement("div");
+     noteStatus.className = "small-status";
+     noteStatus.textContent = individualDoc.data().status;
 
      let noteDate = document.createElement("div");
-     noteDate.className= "notes-small-updated";
+     noteDate.className = "notes-small-updated";
      noteDate.textContent = individualDoc.data().lastEdited;
      
+     noteFooter.appendChild(noteStatus);
+     noteFooter.appendChild(noteDate);
      parentDiv.appendChild(noteTitle);
      parentDiv.appendChild(noteContent);
-     parentDiv.appendChild(noteDate);
+     parentDiv.appendChild(noteFooter);
      notesList.appendChild(parentDiv);
 
      //Adding selecting event to click
@@ -91,6 +130,7 @@ function renderData(individualDoc){
           }else{
                title.value = individualDoc.data().title;
                body.value = individualDoc.data().note;
+               checkingStatus(individualDoc.data().status);
                parentDiv.classList.add("selected");
           }          
      });
@@ -101,21 +141,25 @@ function renderData(individualDoc){
           const selNote = selNotes[0];
           let id = selNote.getAttribute('data-id');
           updatedTitle = title.value;
-          updatedNote = contentShortening(body.value); 
+          updatedNote = body.value; 
           updatedDate = fullDate;
+          updatedStatus = statusOutput();
           console.log(noteTitle);
           console.log(updatedNote);
+          console.log(updatedStatus);
           console.log(updatedDate);
           auth.onAuthStateChanged(user => {
                if(user) {
                     fs.collection(user.uid + "_notes").doc(id).update({
                          title: updatedTitle,
                          note: updatedNote,
+                         status: updatedStatus,
                          lastEdited: updatedDate
                     }).then(() => {
                          selNote.children[0].textContent = updatedTitle;
-                         selNote.children[1].textContent = updatedNote;
-                         selNote.children[2].textContent = updatedDate;
+                         selNote.children[1].textContent = contentShortening(updatedNote);
+                         selNote.children[2].children[0].textContent = updatedStatus;
+                         selNote.children[2].children[1].textContent = updatedDate;
                          console.log("note updated");
                     });
                     
@@ -131,6 +175,9 @@ function renderData(individualDoc){
           auth.onAuthStateChanged(user => {
                if(user) {
                     fs.collection(user.uid + "_notes").doc(id).delete().then(() => {
+                         title.value = "";
+                         body.textContent = "";
+                         todoOption.checked = true;
                          console.log("note deleted");
                     });
                }
@@ -152,6 +199,7 @@ addNoteButton.addEventListener('click', () => {
                     id: 'nb_' + id,
                     title: "New Note",
                     note: "",
+                    status: "Todo",
                     lastEdited: fullDate
                }).then(() => {
                     console.log('note added');
