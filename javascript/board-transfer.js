3f0b556fbd6859ed1d23dfeb9c0579a1f7c2ddc8
king525dev <109004601+king525dev@users.noreply.github.com>
2023-01-04 03:40:46 +0100

    Added Navavigation Menu

---
+++ javascript/board-transfer.js
@@ -0,0 +1,7 @@
+/*===============================
+     [  BOARD and PROJECT ].html
+================================*/ 
+
+window.prjID = "money";
+const projectTitle = document.getElementById("prj-title");
+const projectBody = document.getElementById("prj-body"); 
