defaa75aea5b297066029d0d8954a07dfa509a3a
king525dev <109004601+king525dev@users.noreply.github.com>
2023-01-06 17:51:51 +0100

    Added Executable Format
    Added the .exe version of Taskmaster.

---
+++ extras/Distribution/Taskmaster_Desktop/executable/backend/Taskmaster.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDp+ZSajGE6GIrAP4/z0/9akjWk0esUDVZab+buKJfMv2nfLX7MO4klmv8IYJA5aQiaqfQR5hG9RomGXC8KEpx3gWXSt71gkCWx2knfZgyUHVNJ7kc4CwDS77nLbmrYbxXfvYqwWEGD01a1qNMw8yQunY0TJiadQWLvnaLLnTT/LLAk=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDp+ZSajGE6GIrAP4/z0/9akjWk0esUDVZab+buKJfMv2nfLX7MO4klmv8IYJA5aQiaqfQR5hG9RomGXC8KEpx3gWXSt71gkCWx2knfZgyUHVNJ7kc4CwDS77nLbmrYbxXfvYqwWEGD01a1qNMw8yQunY0TJiadQWLjrc7iJ
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+START https://task-master-e14a8.web.app/
