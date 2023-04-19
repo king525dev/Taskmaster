bd85cac36e6e22d7083e8b51f6d565274a83e0cc
king525dev <109004601+king525dev@users.noreply.github.com>
2023-04-20 00:12:03 +0100

    Modified Toast UI
    Fixed bugs related to the toast UI

---  public/resources/help__[high-order].js
+++  public/resources/help__[high-order].js
@@ -16,42 +16,42 @@ const companies= [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-for(let i = 0; i < companies.length; i++) {
-  console.log(companies[i]);
-}
+// for(let i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+// }
 
 // forEach
 
-companies.forEach(function(company) {
-  console.log(company.name);
-});
+// companies.forEach(function(company) {
+//   console.log(company.name);
+// });
 
 // filter
 
 // Get 21 and older
 
-let canDrink = [];
-for(let i = 0; i < ages.length; i++) {
-  if(ages[i] >= 21) {
-    canDrink.push(ages[i]);
-  }
-}
+// let canDrink = [];
+// for(let i = 0; i < ages.length; i++) {
+//   if(ages[i] >= 21) {
+//     canDrink.push(ages[i]);
+//   }
+// }
 
-const canDrink = ages.filter(function(age) {
-  if(age >= 21) {
-    return true;
-  }
-});
+// const canDrink = ages.filter(function(age) {
+//   if(age >= 21) {
+//     return true;
+//   }
+// });
 
 const canDrink = ages.filter(age => age >= 21);
 
 // Filter retail companies
 
-const retailCompanies = companies.filter(function(company) {
-  if(company.category === 'Retail') {
-    return true;
-  }
-});
+// const retailCompanies = companies.filter(function(company) {
+//   if(company.category === 'Retail') {
+//     return true;
+//   }
+// });
 
 const retailCompanies = companies.filter(company => company.category === 'Retail');
 
@@ -66,19 +66,19 @@ const lastedTenYears = companies.filter(company => (company.end - company.start 
 // map
 
 // Create array of company names
-const companyNames = companies.map(function(company) {
-  return company.name;
-});
+// const companyNames = companies.map(function(company) {
+//   return company.name;
+// });
 
-const testMap = companies.map(function(company) {
-  return `${company.name} [${company.start} - ${company.end}]`;
-});
+// const testMap = companies.map(function(company) {
+//   return `${company.name} [${company.start} - ${company.end}]`;
+// });
 
-const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+// const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
 
-const ageMap = ages
-  .map(age => Math.sqrt(age))
-  .map(age => age * 2);
+// const ageMap = ages
+//   .map(age => Math.sqrt(age))
+//   .map(age => age * 2);
 
 
 
@@ -86,40 +86,40 @@ const ageMap = ages
 
 // Sort companies by start year
 
-const sortedCompanies  = companies.sort(function(c1, c2) {
-  if(c1.start > c2.start) {
-    return 1;
-  } else {
-    return -1;
-  }
-});
+// const sortedCompanies  = companies.sort(function(c1, c2) {
+//   if(c1.start > c2.start) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
 
-const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+// const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
 
 // Sort ages
-const sortAges = ages.sort((a, b) => a - b);
+// const sortAges = ages.sort((a, b) => a - b);
 
-console.log(sortAges);
+// console.log(sortAges);
 
 
 // reduce
 
-let ageSum = 0;
-for(let i = 0; i < ages.length; i++) {
-  ageSum += ages[i];
-}
+// let ageSum = 0;
+// for(let i = 0; i < ages.length; i++) {
+//   ageSum += ages[i];
+// }
 
-const ageSum = ages.reduce(function(total, age) {
-  return total + age;
-}, 0);
+// const ageSum = ages.reduce(function(total, age) {
+//   return total + age;
+// }, 0);
 
-const ageSum = ages.reduce((total, age) => total + age, 0);
+// const ageSum = ages.reduce((total, age) => total + age, 0);
 
-Get total years for all companies
+// Get total years for all companies
 
-const totalYears = companies.reduce(function(total, company) {
-  return total + (company.end - company.start);
-}, 0);
+// const totalYears = companies.reduce(function(total, company) {
+//   return total + (company.end - company.start);
+// }, 0);
 
 const totalYears = companies.reduce((total, company) => total + (company.end - company.start), 0);
 
